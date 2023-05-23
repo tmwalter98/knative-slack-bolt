74f55f55ec4ebe318def176af9b43f4775e7591d
Timothy Walter <tmwalter98@gmail.com>
2023-05-23 06:51:56 -0400

    Refactor

--- src/app.py
+++ src/app.py
@@ -1,122 +1,20 @@
-import json
 import logging
 import os
 
-from aiohttp import web
-from cloudevents.http import from_http
-from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
-from slack_sdk.errors import SlackApiError
-
-from blocks_machine import build_notification_block
-from bot_app import app
-from data_engine import DataEngine
+from bolt_app import KnativeSlackBolt
 
 logging.basicConfig(level=logging.INFO)
 
-data_engine = DataEngine(os.environ["POSTGRES_URL"])
 
+def main():
+    app = KnativeSlackBolt(
+        slack_bot_token=os.environ["SLACK_BOT_TOKEN"],
+        slack_app_token=os.environ["SLACK_APP_TOKEN"],
+        postgres_url=os.environ["POSTGRES_URL"],
+        channel_id=os.environ["CHANNEL_ID"],
+    )
+    app.run_app(port=os.environ.get("PORT", 8080))
 
-async def healthcheck(_req: web.Request) -> web.Response:
-    """Returns OK if app is active."""
-    if socket_mode_client is not None and socket_mode_client.is_connected():
-        return web.Response(status=200, text="OK")
-    return web.Response(status=503, text="The Socket Mode client is inactive")
-
-
-async def cloudevent(_req: web.Request) -> web.Response:
-    def unmarshaller(v):
-        if isinstance(v, bytes):
-            return v.decode("utf-8")
-
-    bytestr_data = await _req.read()
-    # event = from_http(_req.headers, bytearray(bytestr_data), data_unmarshaller=unmarshaller)
-    event = from_http(_req.headers, bytestr_data, data_unmarshaller=unmarshaller)
-    event_data = json.loads(event.data)
-
-    channel_id = "C03K73L2CQL"
-    notification_id = event_data["payload"]["after"]["id"]
-    try:
-        app.logger.info(f"Received notification ID: {notification_id}")
-        (
-            variant_change,
-            variant,
-            product,
-        ) = await data_engine.get_notification_related_objects(notification_id)
-
-        featured_image = (
-            variant.featured_image.get("src", False)
-            if variant.featured_image and variant.featured_image.get("src", False)
-            else await data_engine.get_featured_image(product.id, variant.id)
-        )
-
-        notable_changes = await data_engine.get_notable_changes(variant_change)
-        app.logger.error(
-            f"Change set: {' '.join([f'{k}: {v}' for k, v in notable_changes.items()])}"
-        )
-
-        message_title_components = [product.title]
-        message_title_components.append(
-            variant.title if variant.title != "Default Title" else None
-        )
-
-        message_title_updates = []
-        if "available" in notable_changes:
-            availability = "available" if variant.available else "unavailable"
-            message_title_updates.append(f"now {availability}!")
-        if "price" in notable_changes:
-            d_price = (
-                "drop"
-                if notable_changes["price"][0] > notable_changes["price"][1]
-                else "increase"
-            )
-            message_title_updates.append(f"price {d_price}!")
-        message_title_components.append(
-            " with ".join(message_title_updates) if len(message_title_updates) else None
-        )
-        message_title = " ".join(list(filter(None.__ne__, message_title_components)))
-
-        # Call the chat.postMessage method using the WebClient
-        result = await app.client.chat_postMessage(
-            channel=channel_id,
-            text=message_title,
-            blocks=build_notification_block(
-                product, variant, message_title, featured_image, notable_changes
-            ),
-        )
-
-        await data_engine.mark_notification_delivered(
-            notification_id, result.status_code == 200
-        )
-
-    except SlackApiError as e:
-        app.logger.error(f"Error posting message: {e}")
-
-    return web.Response(status=200, text=json.dumps(event, indent=4, default=str))
-
-
-web_app = app.web_app()
-web_app.add_routes(
-    [
-        web.get("/healthz", healthcheck),
-        web.post("/cloudevents", cloudevent),
-    ]
-)
-
-#
-# Start the app
-#
 
 if __name__ == "__main__":
-
-    async def start_socket_mode(_web_app: web.Application):
-        handler = AsyncSocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
-        await handler.connect_async()
-        global socket_mode_client
-        socket_mode_client = handler.client
-
-    async def shutdown_socket_mode(_web_app: web.Application):
-        await socket_mode_client.close()
-
-    web_app.on_startup.append(start_socket_mode)
-    web_app.on_shutdown.append(shutdown_socket_mode)
-    web.run_app(app=web_app, port=int(os.environ.get("PORT", 8080)))
+    main()
--- src/utilities/middleware.py
+++ src/utilities/middleware.py
@@ -1,3 +1,59 @@
-def log_request(logger, body, next):
-    logger.debug(body)
-    return next()
+import json
+import logging
+from typing import TYPE_CHECKING, Any, Awaitable
+
+from aiohttp import web
+from cloudevents.http import from_http
+from path_dict import PathDict
+from slack_bolt import BoltResponse
+from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
+
+if TYPE_CHECKING:
+    from bolt_app import KnativeSlackBolt
+
+
+async def log_request(
+    logger: logging.Logger, body: dict, next: Awaitable[BoltResponse]
+) -> BoltResponse:
+    """Log the incoming request."""
+
+    logger.debug(json.dumps(body, indent=4, default=str))
+    return await next()
+
+
+async def healthcheck_handler(req: web.Request) -> web.Response:
+    """Returns OK if app is ready with the socket mode client connected.
+
+    Args:
+        req (web.Request): The incoming request.
+
+    Returns:
+        web.Response: The response.
+    """
+    app: KnativeSlackBolt = req.app["slack_app"]
+    client: AsyncSocketModeHandler = app.socket_mode_handler.client
+    if client is not None and client.is_connected():
+        return web.Response(status=200, text="OK")
+    return web.Response(status=503, text="The Socket Mode client is inactive")
+
+
+async def cloudevent_handler(req: web.Request) -> web.Response:
+    """Handle incoming CloudEvents.  This is expecting a Cloud Event produced by the Knative Source for Apache Kafka."""
+
+    def unmarshaller(value: Any) -> str:
+        if isinstance(value, bytes):
+            return value.decode("utf-8")
+        return value
+
+    # Process request body
+    bytestr_data = await req.read()
+    event = from_http(req.headers, bytestr_data, data_unmarshaller=unmarshaller)
+    event_data = PathDict(json.loads(event.data))
+    notification_id = event_data["payload", "after", "id"]
+
+    app: KnativeSlackBolt = req.app["slack_app"]
+    status_code = app.handle_cloudevent_notifications(notification_id)
+
+    return web.Response(
+        status=status_code, text=json.dumps(event, indent=4, default=str)
+    )
